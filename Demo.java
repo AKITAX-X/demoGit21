37b9e055dc7e42af502d12358a6ec4860dc10bb1
AKITAX-X <muhammetali.esenkaya@hotmail.com>
2021-10-11 05:03:01 -0700

    dit is mijn demo

---
+++ Demo.java
@@ -0,0 +1,25 @@
+
+public class Demo 
+{
+	public static void main(String [] args)
+	{	
+		Makelaar John = new Makelaar();
+		System.out.println(John);
+	}
+
+}
+
+class Huis
+{
+	int verkopen()
+	{
+		return 7;
+	}
+}
+
+class Makelaar
+	{
+	void huisKopen(int 7, int 9) {
+		return new Huis();
+	}
+}
